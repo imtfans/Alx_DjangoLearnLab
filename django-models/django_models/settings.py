5ddac35ddeb7be111d21ee6d1fdb2028c4596f46
godsent <godsentojugo@gmail.com>
2025-11-15 17:34:19 +0100

    Add views, templates, and update settings

--- django-models/django_models/settings.py
+++ django-models/django_models/settings.py
@@ -55,13 +55,14 @@ ROOT_URLCONF = 'django_models.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add your templates directory if you have one
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',  # required for admin sidebar
+                'django.contrib.auth.context_processors.auth',  # required for admin
+                'django.contrib.messages.context_processors.messages',  # required for admin
             ],
         },
     },
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from bookshelf.models import Book  # Assuming your Book model is in bookshelf app
 
-# Create your views here.
+# Function-based view
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
