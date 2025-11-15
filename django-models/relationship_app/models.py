ad769f13731b26a2d66179a7335d697b092b991c
godsent <godsentojugo@gmail.com>
2025-11-15 17:02:32 +0100

    feat: add relationship_app with complex models and queries

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
+# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-
+# Book Model
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
 
-
+# Library Model
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
@@ -25,10 +23,13 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-
+# Librarian Model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
         return self.name
+from django.db import models
+
+# Create your models here.
