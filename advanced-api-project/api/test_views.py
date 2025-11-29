376f3834973b241ae217df846bd3df118a1acdb9
godsent <godsentojugo@gmail.com>
2025-11-29 12:38:58 +0100

    updated test_views.py

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,87 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Book
+
+
+class BookAPITests(APITestCase):
+
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='password')
+        
+        # Sample books
+        self.book1 = Book.objects.create(title="Book One", author="Author A", publication_year=2020)
+        self.book2 = Book.objects.create(title="Book Two", author="Author B", publication_year=2021)
+        
+        # URL patterns
+        self.list_url = reverse('book-list')
+        self.detail_url = lambda pk: reverse('book-detail', kwargs={'pk': pk})
+        self.create_url = reverse('book-create')
+        self.update_url = lambda pk: reverse('book-update', kwargs={'pk': pk})
+        self.delete_url = lambda pk: reverse('book-delete', kwargs={'pk': pk})
+
+    # --------------------------
+    # LIST VIEW TESTS
+    # --------------------------
+    def test_book_list(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_book_search(self):
+        response = self.client.get(self.list_url, {'search': 'Book One'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], "Book One")
+
+    def test_book_ordering(self):
+        response = self.client.get(self.list_url, {'ordering': 'publication_year'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['title'], "Book One")  # 2020 comes before 2021
+
+    # --------------------------
+    # DETAIL VIEW TEST
+    # --------------------------
+    def test_book_detail(self):
+        response = self.client.get(self.detail_url(self.book1.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], "Book One")
+
+    # --------------------------
+    # CREATE VIEW TEST
+    # --------------------------
+    def test_create_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)  # login
+        data = {'title': 'Book Three', 'author': 'Author C', 'publication_year': 2022}
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 3)
+        self.assertEqual(Book.objects.get(id=response.data['id']).title, 'Book Three')
+
+    def test_create_book_unauthenticated(self):
+        data = {'title': 'Book Four', 'author': 'Author D', 'publication_year': 2023}
+        response = self.client.post(self.create_url, data, format='json')
+        # If CreateView requires auth, should be 403 Forbidden
+        self.assertIn(response.status_code, [status.HTTP_403_FORBIDDEN, status.HTTP_201_CREATED])
+
+    # --------------------------
+    # UPDATE VIEW TEST
+    # --------------------------
+    def test_update_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        data = {'title': 'Book One Updated', 'author': 'Author A', 'publication_year': 2020}
+        response = self.client.put(self.update_url(self.book1.id), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.title, 'Book One Updated')
+
+    # --------------------------
+    # DELETE VIEW TEST
+    # --------------------------
+    def test_delete_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.delete_url(self.book1.id))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(id=self.book1.id).exists())
