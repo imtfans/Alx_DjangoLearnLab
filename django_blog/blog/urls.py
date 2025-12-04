ea09d97693be51f6cc47d2024957c18999b01cf8
godsent <godsentojugo@gmail.com>
2025-12-04 14:28:26 +0100

    updated CRUD in urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,18 +1,26 @@
 
+    
 
 from django.urls import path
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
+     path('', views.home, name='home'),
     path('login/', views.CustomLoginView.as_view(), name='login'),        # REQUIRED
     path('register/', views.register_view, name='register'),              # REQUIRED
     path('profile/', views.profile_view, name='profile'),                # REQUIRED
 
 
-    path('', views.PostListView.as_view(), name='post-list'),               # /posts/ (root of app)
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
