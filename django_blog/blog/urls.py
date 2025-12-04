0765fa0d7b2d2e59e32bcd3348655256d8222e4b
godsent <godsentojugo@gmail.com>
2025-12-04 12:52:15 +0100

    Updated urls.py on blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,11 @@
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # add other paths here
+    path('login/', views.CustomLoginView.as_view(), name='login'),        # REQUIRED
+    path('register/', views.register_view, name='register'),              # REQUIRED
+    path('profile/', views.profile_view, name='profile'),                # REQUIRED
 ]
