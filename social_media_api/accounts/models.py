fd89f05fdd4b67da786cb3ee45853b40dc7ad182
godsent <godsentojugo@gmail.com>
2025-12-12 12:24:16 +0100

    fix error on model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,17 +1,30 @@
-
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
+    # Avoid reverse accessor clashes
     groups = models.ManyToManyField(
         Group,
-        related_name="custom_user_set",  # change from default 'user_set'
+        related_name="custom_user_set",
         blank=True,
         help_text='The groups this user belongs to.'
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name="custom_user_permissions_set",  # change from default
+        related_name="custom_user_permissions_set",
         blank=True,
         help_text='Specific permissions for this user.'
     )
+
+    # Social media fields
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
