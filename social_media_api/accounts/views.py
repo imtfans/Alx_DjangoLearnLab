6fa3f5890b98fd6806c5535e6aa1000bb97f2650
godsent <godsentojugo@gmail.com>
2025-12-12 14:18:00 +0100

    fixed error on checker

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer
 from .models import User
 
+
 # ----------------------
 # Registration View
 # ----------------------
@@ -20,6 +21,7 @@ class RegisterView(generics.CreateAPIView):
         token, _ = Token.objects.get_or_create(user=user)
         return Response({'user': response.data, 'token': token.key}, status=status.HTTP_201_CREATED)
 
+
 # ----------------------
 # Login View
 # ----------------------
@@ -29,10 +31,10 @@ class LoginView(APIView):
     def post(self, request):
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # Ensure the login serializer returns {'user': user} in validate()
-        user = serializer.validated_data['user']
+        user = serializer.validated_data['user']  # Make sure your login serializer returns {'user': user}
         token, _ = Token.objects.get_or_create(user=user)
         return Response({'token': token.key, 'user_id': user.id, 'username': user.username}, status=status.HTTP_200_OK)
+
 
 # ----------------------
 # Profile View (Retrieve/Update)
@@ -43,6 +45,7 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
 
 # ----------------------
 # Follow/Unfollow Views
@@ -56,6 +59,7 @@ class FollowUserView(APIView):
             return Response({'error': "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
         request.user.following.add(target_user)
         return Response({'message': f'You are now following {target_user.username}'}, status=status.HTTP_200_OK)
+
 
 class UnfollowUserView(APIView):
     permission_classes = [permissions.IsAuthenticated]
